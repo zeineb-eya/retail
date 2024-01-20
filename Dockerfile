fc61b56388ec13173392abc73dad7e8ca01957d1
zeineb.rahmani <zeineb.rahmani@wecraft.tn>
2024-01-20 17:08:38 +0100

    feat: Quality Checks for BigQuery Dataset

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM quay.io/astronomer/astro-runtime:8.8.0
+
+RUN python -m venv soda venv && source soda_venv/bin/activate && \
+    pip install --nocach-dir soda-core-bigquery==3.0.45 && \
+    pip install --nocach-dir soda-core-scientific==3.0.45 && deactivate
--- dags/retail.py
+++ dags/retail.py
@@ -44,5 +44,12 @@ def retail():
         ),
         use_native_support=False,
     )
+    
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
 
+        return check(scan_name, checks_subpath)
+    check_load()
+    
 retail()
