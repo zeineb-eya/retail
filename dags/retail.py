deeed8a61cab22d29c0a47b26e7f22670c651f1a
zeineb.rahmani <zeineb.rahmani@wecraft.tn>
2024-01-20 19:40:14 +0100

    Feat: Implement DBT Reports: Customer, Product, Year

--- dags/retail.py
+++ dags/retail.py
@@ -75,5 +75,15 @@ def retail():
         return check(scan_name, checks_subpath)    
     check_transform()
     
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )
+    
     
 retail()
